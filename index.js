3fd321fc186a9f303ed03121e218bdd7aeb42fb7
kaikaci12 <somewonesomewone@gmail.com>
2024-09-03 17:32:09 +0400

    add file upload handler which responds with the file name,type and size

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {});
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  try {
+    const file = req.file;
+    res.status(200).json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
